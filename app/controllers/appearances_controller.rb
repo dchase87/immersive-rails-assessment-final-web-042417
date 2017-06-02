d98da7f09c913f23544daf1de7c3444f37095900
dchase87 <david.chase4@gmail.com>
2017-06-02 11:07:51 -0400

    Done.

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -6,17 +6,28 @@ class AppearancesController < ApplicationController
 
   def create
     @appearance = Appearance.new(appearance_params)
-    if @appearance.save
-      redirect_to @appearance.episode
+    if @appearance.valid?
+      @appearance.save
+      redirect_to episode_path(@appearance.episode)
     else
       redirect_to new_appearance_path
     end
   end
 
+  def edit
+    @appearance = Appearance.find(params[:id])
+  end
+
+  def update
+    @appearance = Appearance.find(params[:id])
+    @appearance.update(rating: params[:rating], guest_id: @appearance.guest_id, episode_id: @appearance.episode_id, user_id: @appearance.user_id)
+    redirect_to guest_path(@appearance.guest)
+  end
+
   private
 
   def appearance_params
-    params.require(:appearance).permit(:guest_id, :episode_id, :rating)
+    params.require(:appearance).permit(:guest_id, :episode_id, :rating, :user_id)
   end
 
 end
